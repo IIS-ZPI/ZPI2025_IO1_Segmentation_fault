5a1fb13730c9aa57a65281480b841a3626193d9b
Albert <148986780+albertglebocki-pl@users.noreply.github.com>
2026-03-25 19:05:45 +0100

    Added comments to arithmetic_methods.py explaining the existing classes.

--- arithmetic_methods.py
+++ arithmetic_methods.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
 
+# Interface defining an addition operation
+
 class IArithmeticsAdd(ABC):
     @abstractmethod
     def addition(self, A: float, B: float) -> float:
         pass
+
+# Implementation of the addition interface
 
 class ArithmeticsAdd(IArithmeticsAdd):
     def addition(self, A: float, B: float) -> float:
