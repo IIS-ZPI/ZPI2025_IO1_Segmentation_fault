6a9048aa4af6af1f0a5628a9fe52a2a19d33f7b3
Filip C <252788@edu.p.lodz.pl>
2026-03-25 17:41:11 +0100

    added arithmetic division interface and implementation

--- arithmetic_methods.py
+++ arithmetic_methods.py
@@ -8,3 +8,12 @@ class IArithmeticsAdd(ABC):
 class ArithmeticsAdd(IArithmeticsAdd):
     def addition(self, A: float, B: float) -> float:
         return A + B
+
+class IArithmeticsDiv(ABC):
+    @abstractmethod
+    def division(self, A: float, B: float) -> float:
+        pass
+
+class ArithmeticsDiv(IArithmeticsDiv):
+    def division(self, A: float, B: float) -> float:
+        return A / B
