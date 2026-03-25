c261f2db508d873290254b89761d1aefa854b420
VladaParkhomenko <252503@edu.p.lodz.pl>
2026-03-25 22:47:40 +0100

    Move comment 2

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 print("Segmentation_Fault DevOps MrFiFiX")
+#Komentarz 2 dla drugiej gałęzi, zadanie 5 (252503, annvld_uni)
 print("albertglebocki-pl")
 
 
 
 
 
-
-#Komentarz 2 dla drugiej gałęzi, zadanie 5 (252503, annvld_uni)
