477ce6f60d01a95c6433831c9962a2a78e400545
Filip C <252788@edu.p.lodz.pl>
2026-03-26 07:18:09 +0100

    added comments

--- arithmetic_methods.py
+++ arithmetic_methods.py
@@ -1,19 +1,19 @@
 from abc import ABC, abstractmethod
-
+#com 1
 class IArithmeticsAdd(ABC):
     @abstractmethod
     def addition(self, A: float, B: float) -> float:
         pass
-
+#com 2
 class ArithmeticsAdd(IArithmeticsAdd):
     def addition(self, A: float, B: float) -> float:
         return A + B
-
+#com 1
 class IArithmeticsDiff(ABC):
     @abstractmethod
     def difference(self, A: float, B: float) -> float:
         pass
-
+#com 1
 class ArithmeticsDiff(IArithmeticsAdd):
     def difference(self, A: float, B: float) -> float:
         return A - B
@@ -22,16 +22,16 @@ class IArithmeticsMult(ABC):
     @abstractmethod
     def multiplication(self, A: float, B: float) -> float:
         pass
-
+#com 1
 class ArithmeticsMult(IArithmeticsMult):
     def multiplication(self, A: float, B: float) -> float:
         return A * B
-      
- class IArithmeticsDiv(ABC):
+#com 1
+class IArithmeticsDiv(ABC):
     @abstractmethod
     def division(self, A: float, B: float) -> float:
         pass
-
+#com 1
 class ArithmeticsDiv(IArithmeticsDiv):
     def division(self, A: float, B: float) -> float:
         return A / B
