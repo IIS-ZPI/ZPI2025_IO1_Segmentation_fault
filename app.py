e4e21d8df2637f470d0320a99ac81822c7137b2c
Filip Cielibała <252788@edu.p.lodz.pl>
2026-06-24 03:30:54 +0200

    added base dir

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import sys
 
 from streamlit.web import cli
 
+BASE_DIR = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+os.chdir(BASE_DIR)
+
 sys.argv = [
     "streamlit",
     "run",
-    os.path.abspath("init.py"),
+    os.path.join(BASE_DIR, "init.py"),
 ]
 
 sys.exit(cli.main())
